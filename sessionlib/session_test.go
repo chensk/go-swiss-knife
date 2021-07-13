aa873e716329239704313901bb35b37b8f3ee6df
chenshengkun.dean <chenshengkun.dean@bytedance.com>
2021-07-13 20:48:24 +0800

    fix test

--- sessionlib/session_test.go
+++ sessionlib/session_test.go
@@ -30,6 +30,17 @@ func TestSession(t *testing.T) {
 	session.Set("test_key", "test_value")
 	//_ = session.Save(context.Background())
 
+	session, err = CreateSession(func() string {
+		return sessionId
+	}, func(sid string) {
+		sessionId = sid
+	}, []SessionOptions{
+		WithRedisPsm("bytedance.redis.dolphin"),
+		WithRedisTimeout(1 * time.Second),
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
 	v, ok = session.Get("test_key")
 	if !ok {
 		t.Error("key not exist")
